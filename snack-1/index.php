ccfd4064dd7b13ac819a5689acab12528ab61016
GioMench <giorgiaglm@gmail.com>
2024-04-15 12:44:29 +0200

    correction

--- snack-1/index.php
+++ snack-1/index.php
@@ -30,14 +30,33 @@ $squads =[
 ];
 
 
-foreach ($squads as $squad){
-  foreach ($squad as $x => $y) {
-    echo "$y <br>";
-    }
-}
-
-
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php foreach ($squads as $squad) : ?>
+        
+
+        <p>
+        <?= $squad["home"] ?> - <?= $squad["guest"]?>
+        </p>
+        <p>
+        <?= $squad["homepoint"] ?> | <?= $squad["guestpoint"]?>
+        </p>
+    
+    
+    <?php endforeach; ?>
+    
+    
+
+
+</body>
+</html>
